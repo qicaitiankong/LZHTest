b7510cd32ef7e1f42337a380a50a79de47ef3828
qicaitiankong <zihaoshuhan@163.com>
2016-08-07 17:26:57 +0800

    branch17.26

--- LZHTest/ViewController.m
+++ LZHTest/ViewController.m
@@ -16,6 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor grayColor];
     //01
     // Do any additional setup after loading the view, typically from a nib.
 }
