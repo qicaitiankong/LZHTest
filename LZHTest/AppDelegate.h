4edaac684c55508c6dfefe142d4ed0080954f1cd
qicaitiankong <zihaoshuhan@163.com>
2016-08-07 17:16:35 +0800

    Initial Commit

---
+++ LZHTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LZHTest
+//
+//  Created by 李自豪 on 16/8/7.
+//  Copyright © 2016年 李自豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LZHTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LZHTest
+//
+//  Created by 李自豪 on 16/8/7.
+//  Copyright © 2016年 李自豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
